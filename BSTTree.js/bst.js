cac54a7fe039c2ec9d4d849dfd4b91a7f4e4127b
GaurvPatil <gaurvpatil17@gmail.com>
2023-08-23 12:39:57 +0530

    binary search tree insert newNode

---
+++ BSTTree.js/bst.js
@@ -0,0 +1,43 @@
+class Node {
+    constructor(value) {
+      this.value = value;
+      this.right = null;
+      this.left = null;
+    }
+  }
+  
+  class BST {
+    constructor() {
+      this.root = null;
+    }
+  
+    insert(value) {
+      const newNode = new Node(value);
+      if (this.root === null) {
+        this.root = newNode;
+        return this
+      }
+  
+      let temp = this.root
+  
+      while(true){
+          if(newNode.value === temp.value) return false;
+          if(newNode.value < temp.value){
+              if(temp.left === null){
+                  temp.left = newNode;
+                  return this
+              }
+              temp = temp.left
+          }else{
+              if(temp.right === null){
+                  temp.right = newNode;
+                  return this
+              }
+              temp = temp.right
+          }
+      }
+    }
+    
+  }
+  
+  const myBST = new BST();

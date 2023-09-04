3753fd15001a189964fd250079ca055c2a666daa
GaurvPatil <gaurvpatil17@gmail.com>
2023-09-04 13:33:14 +0530

    Missing number in array

---
+++ Array/missingNum.js
@@ -0,0 +1,9 @@
+
+function missingNumber(array, n) {
+    const expectedSum  = ( n * (n+1)) /2;
+    const actualSum = array.reduce((acc , num)=> acc+num , 0);
+    return expectedSum - actualSum
+  }
+  
+  console.log(missingNumber([1, 5, 3, 2,4,6,7], 8));
+  
